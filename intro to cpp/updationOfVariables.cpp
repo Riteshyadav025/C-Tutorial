7cb37bde5253fc77ae61054c24fe380aaaded7da
Riteshyadav025 <deveasy44@gmail.com>
2026-08-14 23:11:15 +0530

    .

---
+++ intro to cpp/updationOfVariables.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int x = 5;
+  cout<<x<<endl;
+  x = 8;
+  cout<<x<<endl;
+  // x = x + 2;  // x = 10
+  x += 2;
+  cout<<x<<endl;
+  // x = x - 6;  // x = 4
+  x -= 6;
+  cout<<x<<endl;
+  // x = x * 5;  // x = 20
+  x *= 5;
+  cout<<x<<endl;
+
+}
---
+++ intro to cpp/variables.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+  // int x; // declaration
+  // x = 5; // initialization
+  // int y = 9; // both
+
+  int x = 5, y = 9;
+  cout<<x*y;
+}
