f72f8fb604f5feafdd98ab9cbe8d44fe3622f7de
Riteshyadav025 <deveasy44@gmail.com>
2026-08-18 22:59:00 +0530

    .

---
+++ CHP-3 Loops/03_PrintEvenNum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  // // 100 times work
+  // for(int i=1;i<=100;i++){
+  //   if(i%2==0) cout<<i<<" ";
+  // }
+
+  // 50 times work
+  for(int i=2;i<=100;i=i+2){
+    cout<<i<<" ";
+  }
+}
