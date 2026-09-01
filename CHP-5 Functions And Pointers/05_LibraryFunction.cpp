8a3beb9f0158375cc2197c42531a2e28f4bd9938
Riteshyadav025 <deveasy44@gmail.com>
2026-09-01 23:13:02 +0530

    .

---
+++ CHP-5 Functions And Pointers/05_LibraryFunction.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+ cout<<cbrt(9)<<endl; 
+ cout<<sqrt(4)<<endl;  // library function
+ cout<<min(4,8)<<endl;
+ cout<<max(4,8)<<endl;
+ cout<<pow(2,10)<<endl;
+}
